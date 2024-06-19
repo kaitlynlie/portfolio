dc61473d82fcaeaf18ce9d3406dbf46b85488490
Kaitlyn Li <87097847+kaitlynlie@users.noreply.github.com>
2024-06-18 23:36:49 -0700

    linked websites, added animations

--- pages/AboutMe.tsx
+++ pages/AboutMe.tsx
@@ -29,7 +29,11 @@ const AboutMe = () => {
             <h2>#about</h2>
             <p>
               My interest started out with creating silly storytelling games as a child, using a language called Donacode, which is similar to Scratch.
-              <br/><br/> Fast forward to today, I've had the privilege of working at a student-led organization that builds products for students and a food pantry website to ensure no student is hungry. I am currently interning as a Software Engineer Intern at Roche, and I'm grateful for this opportunity to grow and help more people outside of my college.
+              <br/><br/> Fast forward to today, I've had the privilege of working at a&nbsp;
+              <a href="https://aggieworks.org" target="_blank" rel="noopener noreferrer">student-led organization that builds products for students</a>
+              &nbsp;and&nbsp;
+              <a href="https://order.thepantry.ucdavis.edu" target="_blank" rel="noopener noreferrer">a food pantry website to ensure no student is hungry</a>
+              . I am currently interning as a Software Engineer Intern at Roche, and I'm grateful for this opportunity to grow and help more people outside of my college.
               {/* <br/><br/> Here are a few technologies I have been working with recently: */}
               {/* <ul className={clsx(styles.list)}>
                 <li>Typescript</li>
