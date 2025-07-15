57866135b55012bc90446bd6976ae8b7cf2e2122
Kaitlyn Li <87097847+kaitlynlie@users.noreply.github.com>
2025-07-14 20:47:23 -0700

    lint

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["warn"],
+  },
+};
